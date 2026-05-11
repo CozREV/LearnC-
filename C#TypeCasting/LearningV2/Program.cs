5000cf4cd1ea0fd083053b55f2a00dccfef18c5a
CozREV <kristianafg@proton.me>
2026-05-11 12:28:23 +0200

    Add C# TypeCasting and VariablesConstants demos
    Introduce two new sample projects demonstrating basic C# concepts: TypeCasting and VariablesConstants. Each project includes a solution file, a net10.0 csproj (with implicit usings and nullable enabled), and a Program.cs: TypeCasting shows Convert.* usages (ToInt32, ToDouble, ToString, ToChar, ToBoolean) and prints resulting types; VariablesConstants demonstrates declaration/initialization, basic types, concatenation, and a const usage. These are educational/example files for learning C# basics.

---
+++ C#TypeCasting/LearningV2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+/* Type Casting = Converting a value to a different data type
+ *                Useful when we accept user input (string)
+ *                Different data types can do different things
+*/
+
+
+
+// Turns a to b, but it will round the number to the nearest integer
+double a = 3.14; // "double a" is a number with a decimal point
+int b = Convert.ToInt32(a); // "int b" rounds "double a" to the nearest integer with Convert.ToInt32()
+
+// Turns c to d, but it will keep the decimal point and add 0.1 to it
+int c = 123; // "int c" is a whole number without a decimal point
+double d = Convert.ToDouble(c) + 0.1; // "double d" converts "int c" to a number with a decimal point and adds 0.1 to it
+
+// Turns e to f, but it will convert the number to a string
+int e = 321; // "int e" is a whole number without a decimal point
+String f = Convert.ToString(e); // "String f" converts "int e" to a sequence of characters (text) that represents the number
+
+// Turns g to h, but it will convert the string to a single character
+String g = "#"; // "String g" is a sequence of characters (text) that represents the symbol "#"
+char h = Convert.ToChar(g); // "char h" converts "String g" to a single character that represents the symbol "#"
+
+// Turns i to j, but it will convert the string to a boolean value
+String i = "false"; // "String i" is a sequence of characters (text) that represents the word "false"
+bool j = Convert.ToBoolean(i); // "bool j" converts "String i" to a boolean value that represents the concept of "false" (not true)
+
+
+Console.WriteLine(b.GetType());
+Console.WriteLine(d.GetType());
+Console.WriteLine(f.GetType());
+Console.WriteLine(h.GetType());
+Console.WriteLine(j.GetType());
+
+
+Console.ReadKey();
---
+++ C#VariablesConstants/learning/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.Metadata;
+
+int x; //declaration
+x = 123; //initialization
+
+int y = 321; //declaration + initialization
+
+int z = x + y;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+
+
+int age = 22; //int stores only whole integer values
+double height = 1.79; //double can store decimal values
+bool alive = true; //bool can store true or false values
+char symbol = '#'; //char can store a single character, enclosed in single quotes
+String name = "REV"; //string can store a sequence of characters, enclosed in double quotes
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine("Your age is " + age);
+Console.WriteLine("Your height is " + height + "m");
+Console.WriteLine("Are you alive? " + alive);
+Console.WriteLine("Your symbol is: " + symbol);
+
+
+
+String userName = symbol + name; //concatenating the char symbol and the string name to create a new string userName
+
+Console.WriteLine(userName);
+
+
+
+// constants = immutable values which are known at compile time and do not change for the life of the program
+
+const double pi = 3.14159; //const keyword is used to declare a constant variable, which cannot be changed after it is initialized
+
+// pi = 420; = this line would cause a compile-time error because pi is a constant and cannot be assigned a new value
+
+
+
+Console.ReadKey(); //waits for the user to press a key before closing the console window
