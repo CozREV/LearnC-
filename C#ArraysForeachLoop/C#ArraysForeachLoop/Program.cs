85b85f8373c7a55f738539eaa080f7f95b114682
CozREV <kristianafg@proton.me>
2026-05-18 14:47:33 +0200

    Add C# examples: Arrays foreach and Methods
    Add two new C# sample projects demonstrating basic language features. Each project includes a solution (.slnx), a .csproj targeting net10.0 with implicit usings and nullable enabled, and a Program.cs example: C_ArraysForeachLoop prints a list of car names using a foreach loop; C_Methods defines and invokes a singHappyBirthday method. These are simple educational examples for learning loops and methods.

---
+++ C#ArraysForeachLoop/C#ArraysForeachLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace C_ArraysForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+            String[] cars = { "Honda", "Toyota", "GR GT" };
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#Methods/C#Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // method  = performs a section of code, whenever it's called "invoked".
+            //           benefit = Let's us reuse code w/o writing it multiple times
+            //           Good practice is to capitalize method names
+
+            String name = "REV";
+            int age = 22;
+
+            singHappyBirthday(name, age); 
+
+            Console.ReadKey();
+
+
+
+        }
+        static void singHappyBirthday(String birthdayBoy, int yearsOld)
+        {
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday dear " + birthdayBoy);
+            Console.WriteLine("You are " + yearsOld + " years old!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine();
+        }
+    }
+}
