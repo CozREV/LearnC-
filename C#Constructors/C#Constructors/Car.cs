0747b6b65593ff8d0561d504e9fbf437449399ff
CozREV <kristianafg@proton.me>
2026-05-20 14:54:37 +0200

    Add C#Constructors sample project
    Add a new C# sample demonstrating constructors: create a solution and .csproj targeting .NET 10 with implicit usings and nullable enabled; add Car.cs (fields, constructor, Drive method); add Program.cs which instantiates two Car objects and calls Drive().

---
+++ C#Constructors/C#Constructors/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Constructors
+{
+    internal class Car
+    {
+        String make;
+        String model;
+        int year;
+        String color;
+
+        public Car(String make, String model, int year, String color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"You are driving the {color} {make} {model} {year}.");
+        }
+    }
+}
---
+++ C#Constructors/C#Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // constructor = A special method in a class
+            //               Same name as the class name
+            //               Can be used to assign arguments to fields when creating an object
+
+            Car car1 = new Car("Toyota", "GR GT", 2027, "Red");
+            Car car2 = new Car("Toyota", "Supra MK5", 2023, "Blue");
+
+            car1.Drive();
+            car2.Drive();
+
+            Console.ReadKey();
+        }
+    }
+}
